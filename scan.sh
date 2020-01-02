501bfb1297db7306de90c420363702eae81733fc
liberodark <liberodark@gmail.com>
2020-01-02 15:46:34 +0100

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# About: ClamAV Scan
+# Author: liberodark
+# License: GNU GPLv3
+
+version="0.1.0"
+
+echo "Welcome on ClamAV Scan Script $version"
+
+#=================================================
+# CHECK ROOT
+#=================================================
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+
+#=================================================
+# RETRIEVE ARGUMENTS FROM THE MANIFEST AND VAR
+#=================================================
+
+dest="/"
+lock="/tmp/clamav-scan.lock"
+args=""
+
+# Update ClamAV Def
+freshclam
+
+exec 9>"${lock}"
+flock -n 9 || exit
+
+# Scan
+clamscan -i -r \
+    --exclude-dir= "/var/lib/clamav/*" \
+    --exclude "/usr/local/maldetect/*" \
+    --exclude "/usr/NX/lib/perl/*" \
+    "$dest" "$args"
