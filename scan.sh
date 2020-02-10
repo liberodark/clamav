a66184b331bbc6b237ef5894bdf75c02ad61e823
liberodark <liberodark@gmail.com>
2020-02-10 08:34:30 +0100

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -20,7 +20,7 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 dest="/"
 lock="/tmp/clamav-scan.lock"
-#args=""
+args="-i -r"
 #mail=$(| grep "Infected files" | grep -v "Infected files: 0$" | ifne mail -s clamav_log_`hostname` support@example.com)
 date=$(date +%Y.%m.%d_%H-%M-%S)
 
@@ -50,7 +50,7 @@ exec 9>"${lock}"
 flock -n 9 || exit
 
 # Scan
-clamscan -i -r \
+clamscan "$args" \
     --log="/var/log/clamav/scan-"$date".log" \
     --exclude="/var/lib/clamav/" \
     "$dest"
