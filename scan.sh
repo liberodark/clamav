e5607599038f8199c1c1a657d34c5a0173dd9c87
liberodark <liberodark@gmail.com>
2020-02-11 19:20:13 +0100

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -4,7 +4,7 @@
 # Author: liberodark
 # License: GNU GPLv3
 
-version="0.1.0"
+version="0.1.9"
 
 echo "Welcome on ClamAV Scan Script $version"
 
@@ -25,7 +25,10 @@ lock="/tmp/clamav-scan.lock"
 date=$(date +%Y.%m.%d_%H-%M-%S)
 
 # Check user
-if [ `id -u pc 2>/dev/null || echo -1` -ge 0 ]; then
+if [ "$(id -u clamav 2>/dev/null || echo -1)" -ge 0 ]; then
+echo "user exist"
+else
+echo "user dont exist"
 groupadd clamav
 useradd -g clamav -s /bin/false -c "Clam Antivirus" clamav
 fi
@@ -56,7 +59,7 @@ flock -n 9 || exit
 
 # Scan
 clamdscan -i --fdpass \
-    --log="/var/log/clamav/scan-"$date".log" \
+    --log="/var/log/clamav/scan-""$date"".log" \
     "$dest" --move="$tmp_folder"
 
 # Virus
